e9659d488c06d5f63a84c3fe9d4c17d1b28fa358
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-12-05 15:52:32 -0300

    fix: arrays, models object pokemon

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,7 +1,14 @@
+//Abaixo Objeto pokemon recebe os seguintes atribus vindos da Api.
+
 class Pokemon {
     number;
     name;
     type;
     types = [];
     photo;
+    stats = [];
+    abilities = [];
+    species = [];
+    height;
+    wieght;
 }
