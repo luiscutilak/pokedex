71330764182a9b94879d0c4d44b886674eea166a
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-12-06 09:37:31 -0300

    fix error addEventListener is not a function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-const pokemonList = document.getElementById('pokemonList'); //aqui pegamos somente a lista de pokemons da <ol> no HTML
+
+
+const pokemonListHtml = document.getElementsByClassName('pokemons'); //aqui pegamos somente a lista de pokemons da <ol> no HTML
 const loadMoreButton = document.getElementById('loadMoreButton'); //aqui carregamos a nova lista cada vez que é clicado no botao load more.
 const pokemon = document.getElementsByClassName('pokemon'); //aqui vamos pegar tods os elementos la do html na lista <li> da classe pokemon.
 
@@ -25,11 +27,37 @@ function convertPokemonToLi(pokemon) {
 `
 };
 
-// chamando a escuta do evento de click no HTML, e abrindo um modal com as novas caracteristicas(CARD dos STATS)
-pokemonList.addEventListener('click', function(event) {
-    const clickedPokemon = event.target.closest('.pokemon');
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit)
+        .then((pokemons = []) => {
+            const newHtml = pokemons.map(convertPokemonToLi).join('');
+            pokemonList.innerHTML += newHtml;
+        });
+};
 
-    if (typeof clickedPokemon !== null) {
+loadPokemonItens(offset, limit);
+
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit;
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+
+    } else {
+        loadPokemonItens(offset, limit);
+    };
+});
+
+// chamando a escuta do evento de click no HTML, e abrindo um modal com as novas caracteristicas(CARD dos STATS)
+
+pokemonListHtml.addEventListener('click', (event) => {
+    const clickedPokemon = event.target.closest('.pokemon');
+    
+    if (clickedPokemon) {
 
         const pokemonData = JSON.parse(clickedPokemon.getAttribute('data-pokemon'));  //se o pokemon foi cliclado, entra no if JSON.parse() transforma a string JSON em um objeto para ser manipulado
         const pokemonName = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1); //atribuindo o retorno do JSON transformado em objto e manipulando  nome, primeira letra(charAt), toUpperCase em letra maiuscula, name.slice(1), retira as duas primeiras chaves do objeto.
@@ -37,7 +65,7 @@ pokemonList.addEventListener('click', function(event) {
         const modalBody = document.getElementById('pokemonModalBody'); // construindo Modal 
 
         modalBody = document.getElementById('pokemonModalBody');
-          
+
         //Transformando para Arquivo Texto, inserindo no HTML, com as especialidades de cada pokemon, dinamicamente.
         modalBody.innerHTML = `
             
@@ -83,30 +111,7 @@ pokemonList.addEventListener('click', function(event) {
                 </table>                        
             </div>               
         </div>
-    `;
-    modal.show();
-    }
+    `
+        modal.show();
+    };
 });
-
-function loadPokemonItens(offset, limit) {
-                    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-                        const newHtml = pokemons.map(convertPokemonToLi).join('')
-                        pokemonList.innerHTML += newHtml
-                    })
-}
-
-    loadPokemonItens(offset, limit)
-
-loadMoreButton.addEventListener('click', () => {
-                    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
-
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-                loadPokemonItens(offset, newLimit)
-                loadMoreButton.parentElement.removeChild(loadMoreButton)
-        
-    } else {
-                    loadPokemonItens(offset, limit)
-                }
-})
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -4,12 +4,12 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon();
     pokemon.number = pokeDetail.id;
     pokemon.name = pokeDetail.name;
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
     pokemon.types = types;
     pokemon.type = type;
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
     const abilities = pokeDetail.abilities.map((ability) => ability.ability.name).join(', ');
     pokemon.abilities = abilities;  //abastecendo os ARRAYS da classe pokemon-model. Através do método MAP
     pokemon.species = pokeDetail.species.name;
@@ -28,26 +28,12 @@ pokeApi.getPokemonDetail = (pokemon) => {
 
 // retornando a requisição API e Aguardando A PROMISSE. Retorna toda manipulação do Fetch ou seja do consumo da APi
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
-
-// const searchPokemon = event => {
-//     event.preventDefault();
-//     const { value } = event.target.pokemon;
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
-//     .then(data => data.json())
-//     .then(response => renderPokemonData(response))
-// }
-
-// const renderPokemonData = data => {
-//     const sprite = data.sprites.front_default;
-//     const { stats } = data;
-//     console.log(stats);
-// }
+        .then((pokemonsDetails) => pokemonsDetails);
+};
